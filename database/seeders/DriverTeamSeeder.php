8db17c57e19b1e79af065aefb03a3d3f7a7014d9
Alex <12721617+lolsokje@users.noreply.github.com>
2025-08-19 23:53:06 +0200

    bugfix: fix drivers being assigned to incorrect team in seeder

--- database/seeders/DriverTeamSeeder.php
+++ database/seeders/DriverTeamSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\Driver;
+use App\Models\Series;
 use App\Models\Team;
 use Illuminate\Database\Seeder;
 
@@ -12,16 +13,23 @@ final class DriverTeamSeeder extends Seeder
 {
     public function run(): void
     {
-        /** @var array<array{team_name: string, given_name: string, family_name: string, driver_sheet_id: string|int|null}> $combinations */
+        /** @var array<array{team_name: string, series_name: string, given_name: string, family_name: string, driver_sheet_id: string|int|null}> $combinations */
         $combinations = json_decode(file_get_contents(resource_path('json/driver_team.json')), true);
 
         foreach ($combinations as $combination) {
+            $series = Series::query()
+                ->where('name', $combination['series_name'])
+                ->first();
+
             $driver = Driver::query()
                 ->where('given_name', $combination['given_name'])
                 ->where('family_name', $combination['family_name'])
                 ->first();
 
-            $team = Team::where('short_name', $combination['team_name'])->first();
+            $team = Team::query()
+                ->where('short_name', $combination['team_name'])
+                ->where('series_id', $series->id)
+                ->first();
 
             $team->drivers()->attach($driver, [
                 'season_id' => $team->season_id,
